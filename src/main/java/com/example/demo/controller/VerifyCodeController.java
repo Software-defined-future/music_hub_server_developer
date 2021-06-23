238e21f1b83021080785833ac8795d260e1c6979
JTNTR <724128629@qq.com>
2021-06-23 15:33:27 +0800

    验证码

---
+++ src/main/java/com/example/demo/controller/VerifyCodeController.java
@@ -0,0 +1,38 @@
+package com.example.demo.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.example.demo.util.VerificationCodeUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+import java.awt.image.BufferedImage;
+
+@RestController
+public class VerifyCodeController {
+    @ResponseBody
+    @GetMapping(value = "/Captcha.jpg")
+    public Object getCaptcha(){
+        JSONObject jsonObject = new JSONObject();
+        try{
+            VerificationCodeUtil captcha = new VerificationCodeUtil();
+            StringBuffer code = new StringBuffer();
+            BufferedImage image = captcha.genRandomCodeImage(code);
+            String base64 = captcha.BufferedImageToBase64(image);
+            jsonObject.put("image",base64);
+            jsonObject.put("code",1);
+            jsonObject.put("verifyCode",code.toString());
+            return jsonObject;
+        }catch (Exception e){
+            e.printStackTrace();
+            jsonObject.put("code",0);
+            return jsonObject;
+        }
+    }
+
+
+
+}
